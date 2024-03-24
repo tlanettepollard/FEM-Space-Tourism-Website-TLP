29ebd14dab14c1ccfff09e3de2533c8bba102d9d
T. Lanette <TLPollard4516@gmail.com>
2024-03-23 21:38:24 -0400

    Updates: Home Navbar
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -6,37 +6,39 @@ export default function Header() {
     return (
         <>
             {[false].map((expand) => (
-                <Navbar key={expand} expand={expand} className='bg-dark mb-3 navbar-expand-md'>
-                    <Container fluid>
-                        <Navbar.Brand href='#'>
-                            <img src="/public/assets/shared/logo.svg" alt="company logo" />
-                        </Navbar.Brand>
-                        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-                        <Navbar.Offcanvas id={`offcanvasNavbar-expand-${expand}`} className='bg-dark' aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`} placement='end'>
-                            <Offcanvas.Header closeButton />
-                            <Offcanvas.Body>
-                                <Nav className='justify-content-end flex-grow-1 pe-3'>
-                                    <NavLink to='/home' id='home'>
-                                        <span>01</span>
-                                        <span>Home</span>
-                                    </NavLink>
-                                    <NavLink to='/destinations' id='destinations'>
-                                        <span>02</span>
-                                        <span>Destinations</span>
-                                    </NavLink>
-                                    <NavLink to='/crew' id='crew'>
-                                        <span>03</span>
-                                        <span>Crew</span>
-                                    </NavLink>
-                                    <NavLink to='/technology' id='technology'>
-                                        <span>04</span>
-                                        <span>Technology</span>
-                                    </NavLink>
-                                </Nav>
-                            </Offcanvas.Body>
-                        </Navbar.Offcanvas>
-                    </Container>
-                </Navbar>
+                <header className='bg-transparent'>
+                    <Navbar key={expand} expand={expand} className='bg-transparent mb-3 navbar-expand-md'>
+                        <Container fluid>
+                            <Navbar.Brand href='#'>
+                                <img src="/public/assets/shared/logo.svg" alt="company logo" />
+                            </Navbar.Brand>
+                            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+                            <Navbar.Offcanvas id={`offcanvasNavbar-expand-${expand}`} className='bg-dark' aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`} placement='end'>
+                                <Offcanvas.Header closeButton />
+                                <Offcanvas.Body>
+                                    <Nav className='justify-content-end flex-grow-1 pe-3'>
+                                        <NavLink to='/home' id='home'>
+                                            <span>01</span>
+                                            <span>Home</span>
+                                        </NavLink>
+                                        <NavLink to='/destinations' id='destinations'>
+                                            <span>02</span>
+                                            <span>Destinations</span>
+                                        </NavLink>
+                                        <NavLink to='/crew' id='crew'>
+                                            <span>03</span>
+                                            <span>Crew</span>
+                                        </NavLink>
+                                        <NavLink to='/technology' id='technology'>
+                                            <span>04</span>
+                                            <span>Technology</span>
+                                        </NavLink>
+                                    </Nav>
+                                </Offcanvas.Body>
+                            </Navbar.Offcanvas>
+                        </Container>
+                    </Navbar>
+                </header>
             ))}
         </>
     );
