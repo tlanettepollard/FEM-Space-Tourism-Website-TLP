fdfc2d65980a12667dd703b2960f894eebe4e316
T. Lanette <TLPollard4516@gmail.com>
2024-03-23 20:16:20 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Nav, Container, Navbar, NavbarBrand, NavbarCollapse, NavbarToggle } from 'react-bootstrap';
 //import { NavLink } from 'react-router-dom';
 
 export default function Header() {
 
 
     return (
-        <header>
-            <nav> Header</nav>
+        <header className='container-fluid'>
+            <Navbar collapseOnSelect expand='md' className='navbar-dark container-fluid'>
+                <Container>
+                    <NavbarBrand>
+                        <img src="/public/assets/shared/logo.svg" alt="company logo" />
+                    </NavbarBrand>
+                    <NavbarToggle aria-controls='navbarCollapse' id='navbarBtn' className='custom-toggler' />
+                    <NavbarCollapse>
+
+                    </NavbarCollapse>
+                </Container>
+            </Navbar>
         </header>
     );
 }
