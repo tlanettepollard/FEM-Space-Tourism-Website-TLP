6c26f47c92c4e589650deb7a4756e8eb2b7aac84
T. Lanette <TLPollard4516@gmail.com>
2024-03-24 21:31:40 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Nav, Container, Navbar, NavbarBrand, NavbarCollapse, NavbarToggle, Offcanvas } from 'react-bootstrap';
+import { Nav, Container, Navbar, NavbarBrand, NavbarToggle, Offcanvas } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 
 export default function Header() {
@@ -7,15 +7,15 @@ export default function Header() {
         <>
             {[false].map((expand) => (
                 <header className='bg-transparent'>
-                    <Navbar key={expand} expand={expand} className='bg-transparent mb-3 navbar-expand-md'>
+                    <Navbar key={expand} expand={expand} className='bg-transparent mb-3 navbar-expand-md navbar-dark'>
                         <Container fluid>
-                            <Navbar.Brand href='#'>
-                                <img src="/public/assets/shared/logo.svg" alt="company logo" />
-                            </Navbar.Brand>
-                            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-                            <Navbar.Offcanvas id={`offcanvasNavbar-expand-${expand}`} className='bg-dark' aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`} placement='end'>
-                                <Offcanvas.Header closeButton />
-                                <Offcanvas.Body>
+                            <NavbarBrand href='#'>
+                                <img src="/assets/shared/logo.svg" alt="company logo" />
+                            </NavbarBrand>
+                            <NavbarToggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+                            <NavbarOffcanvas id={`offcanvasNavbar-expand-${expand}`} className='bg-dark' aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`} placement='end'>
+                                <OffcanvasHeader closeButton />
+                                <OffcanvasBody>
                                     <Nav className='justify-content-end flex-grow-1 pe-3'>
                                         <NavLink to='/home' id='home'>
                                             <span>01</span>
@@ -34,8 +34,8 @@ export default function Header() {
                                             <span>Technology</span>
                                         </NavLink>
                                     </Nav>
-                                </Offcanvas.Body>
-                            </Navbar.Offcanvas>
+                                </OffcanvasBody>
+                            </NavbarOffcanvas>
                         </Container>
                     </Navbar>
                 </header>
