4eefceaa0c1f4f378e25674b7bc99163cbf214a4
T. Lanette <TLPollard4516@gmail.com>
2024-03-29 22:24:34 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,10 @@ export default function Home() {
                         <p className='body-text'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
                     </article>
                     <div className='btn-container'>
-                        <img className='img-fluid explore-btn' src="/public/assets/home/explorebtn-mobile.png" alt="explore button" />
+                        <picture>
+
+                            <img className='img-fluid explore-btn' src="/public/assets/home/explorebtn-mobile.png" />
+                        </picture>
                     </div>
                 </section>
             </main>
