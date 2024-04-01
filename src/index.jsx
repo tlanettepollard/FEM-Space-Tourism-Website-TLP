12fcadc31cd701afdeedfa39e9f8912dc3ca4721
T. Lanette <TLPollard4516@gmail.com>
2024-04-01 16:38:51 -0400

    Fixed router and darkened crew bg image
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
-);
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
