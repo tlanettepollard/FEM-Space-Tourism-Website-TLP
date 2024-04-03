07dcf68e60e4f3db968151ac497f84ba7376f176
T. Lanette <TLPollard4516@gmail.com>
2024-04-03 13:51:05 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,14 +10,14 @@ export default function Home() {
         <div className='home-page'>
             <Header />
             <main className='home-main mb-5'>
-                <section className='container'>
+                <section className='container d-flex flex-column align-items-center'>
                     <article>
-                        <span className='header-5 text-uppercase'>So, you want to travel to</span>
-                        <h1 className='header-1'>Space</h1>
-                        <p className='body-text'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+                        <p className='header-5 text-uppercase text-center'>So, you want to travel to</p>
+                        <h1 className='header-1 text-center'>Space</h1>
+                        <p className='body-text text-center'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
                     </article>
-                    <div className='home-btn-container mb-5'>
-                        <a className='btn btn-lg text-uppercase text-dark bg-white text-center text-decoration-none d-flex' href="#">
+                    <div className='home-btn-container mt-5'>
+                        <a className='btn btn-lg text-uppercase text-dark bg-white text-center text-decoration-none d-flex position-relative' href="#">
                             <span className='w-100'>Explore</span>
                         </a>
                     </div>
