5d1cf4f43c5392d810852f2063ecad82ce950563
T. Lanette <TLPollard4516@gmail.com>
2024-04-03 13:36:43 -0400

    Updates: font for explore button
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,7 +17,7 @@ export default function Home() {
                         <p className='body-text'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
                     </article>
                     <div className='home-btn-container mb-5'>
-                        <a className='btn btn-lg text-uppercase text-dark fs-1 bg-white text-center text-decoration-none d-flex' href="#">
+                        <a className='btn btn-lg text-uppercase text-dark bg-white text-center text-decoration-none d-flex' href="#">
                             <span className='w-100'>Explore</span>
                         </a>
                     </div>
