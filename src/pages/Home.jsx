1d08e42e0bdd1bccca555ecd7aed70b7987f46b2
T. Lanette <TLPollard4516@gmail.com>
2024-04-03 13:10:48 -0400

    Updates: changed explore button to link
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,17 +9,17 @@ export default function Home() {
     return (
         <div className='home-page'>
             <Header />
-            <main className='home-main'>
+            <main className='home-main mb-5'>
                 <section className='container'>
                     <article>
                         <span className='header-5 text-uppercase'>So, you want to travel to</span>
                         <h1 className='header-1'>Space</h1>
                         <p className='body-text'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
                     </article>
-                    <div className='btn-container'>
-                        <img className='img-fluid explore-btn d-md-none' src='/assets/home/explorebtn-mobile.png' alt='explore button' />
-                        <img className='d-none d-md-block d-lg-none' src="/assets/home/explorebtn-tablet.png" alt="explore button" />
-                        <img className='d-none d-md-none d-lg-block' src="/assets/home/explorebtn-desktop.png" alt="explore button" />
+                    <div className='home-btn-container mb-5'>
+                        <a className='btn btn-lg text-uppercase text-dark fs-1 bg-white text-center text-decoration-none d-flex' href="#">
+                            <span className='w-100'>Explore</span>
+                        </a>
                     </div>
                 </section>
             </main>
