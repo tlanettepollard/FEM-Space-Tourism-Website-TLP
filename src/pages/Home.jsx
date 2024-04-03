4b3d63a36a922c4a05a09da77a93b4a15985ee60
T. Lanette <TLPollard4516@gmail.com>
2024-04-03 16:42:07 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,13 +10,13 @@ export default function Home() {
         <div className='home-page'>
             <Header />
             <main className='home-main mb-5 mb-lg-5'>
-                <section className='container d-flex flex-column align-items-center'>
-                    <article>
-                        <p className='header-5 text-uppercase text-center'>So, you want to travel to</p>
-                        <h1 className='header-1 text-center'>Space</h1>
-                        <p className='body-text text-center'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+                <section className='container d-flex flex-column justify-content-center align-items-center'>
+                    <article className='home-content d-flex flex-column justify-content-center'>
+                        <p className='header-5 text-uppercase text-center home-subheading mt-md-5 pt-md-5'>So, you want to travel to</p>
+                        <h1 className='header-1 text-center home-title my-4'>Space</h1>
+                        <p className='home-text body-text text-center px-5'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
                     </article>
-                    <div className='home-btn-container mt-5 mb-lg-5'>
+                    <div className='home-btn-container mt-5 mb-lg-5 pt-4 pt-md-5'>
                         <a className='btn btn-lg text-uppercase text-dark bg-white text-center text-decoration-none d-flex position-relative' href="#">
                             <span className='w-100'>Explore</span>
                         </a>
