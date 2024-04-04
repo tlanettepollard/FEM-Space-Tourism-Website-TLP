047333e9328198d97a1b27c82783f8eea81c0f2f
T. Lanette <TLPollard4516@gmail.com>
2024-04-03 20:18:33 -0400

    Updates: positioning grid
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
             <Header />
             <main className='home-main'>
                 <section className='grid-container container-fluid'>
-                    <article className='home-content align-self-center mx-auto'>
+                    <article className='home-content align-self-center align-content-xl-between mx-auto'>
                         <h1 className='header-5 text-uppercase text-center text-xl-start home-subheading'>So, you want to travel to</h1>
                         <h2 className='header-1 text-center text-xl-start home-title my-4'>Space</h2>
                         <p className='home-text body-text text-center text-xl-start'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
