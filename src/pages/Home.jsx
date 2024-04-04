fe4594b52529544fde88a889c24c69d6d11cd09c
T. Lanette <TLPollard4516@gmail.com>
2024-04-03 20:15:19 -0400

    Updates: added css grid
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,14 +9,14 @@ export default function Home() {
     return (
         <div className='home-page'>
             <Header />
-            <main className='home-main mb-5 mb-lg-5'>
-                <section className='container d-flex flex-column justify-content-center align-items-center'>
-                    <article className='home-content d-flex flex-column justify-content-center'>
-                        <p className='header-5 text-uppercase text-center home-subheading mt-md-5 pt-md-5'>So, you want to travel to</p>
-                        <h1 className='header-1 text-center home-title my-4'>Space</h1>
-                        <p className='home-text body-text text-center px-5'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+            <main className='home-main'>
+                <section className='grid-container container-fluid'>
+                    <article className='home-content align-self-center mx-auto'>
+                        <h1 className='header-5 text-uppercase text-center text-xl-start home-subheading'>So, you want to travel to</h1>
+                        <h2 className='header-1 text-center text-xl-start home-title my-4'>Space</h2>
+                        <p className='home-text body-text text-center text-xl-start'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
                     </article>
-                    <div className='home-btn-container mt-5 mb-lg-5 pt-4 pt-md-5'>
+                    <div className='home-btn-container d-flex justify-content-center align-self-start align-self-xl-center'>
                         <a className='btn btn-lg text-uppercase text-dark bg-white text-center text-decoration-none d-flex position-relative' href="#">
                             <span className='w-100'>Explore</span>
                         </a>
