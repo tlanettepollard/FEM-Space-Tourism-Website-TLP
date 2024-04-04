a311218c4f8f3d3a1d44594dedda7ec049a2225c
T. Lanette <TLPollard4516@gmail.com>
2024-04-04 19:10:11 -0400

    Updates: adding grid to Destination page
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/pages/Destinations.jsx
+++ src/pages/Destinations.jsx
@@ -9,12 +9,11 @@ export default function Destinations({ data }) {
     return (
         <section className='destination-page container-fluid'>
             <Header />
-            <div className='grid-container container'>
-
-                <article className='dest-image-container align-self-center '>
-                    <h1 className='dest-page-title nav-text text-uppercase pb-3'>
-                        <p className='page-title-number nav-text text-center opacity-25'>01</p> Pick your destination
-                    </h1>
+            <div className='grid-container grid-container--destination container'>
+                <h1 className='numbered-title nav-text text-uppercase pb-3'>
+                    <span aria-hidden='true' className='nav-text opacity-25'>01</span> Pick your destination
+                </h1>
+                <article className='dest-image-container align-self-center  align-self-xl-end'>
                     <picture className='destinations-img'>
                         <source media="(min-width: 992px)" srcSet={data[activeTab].images.webp} />
                         <img className='img-fluid mx-auto d-block' src={data[activeTab].images.png} alt={data[activeTab].name} />
@@ -27,18 +26,18 @@ export default function Destinations({ data }) {
                         activeTab={activeTab}
                         setActiveTab={setActiveTab}
                     />
-                    <div className='mb-5'>
+                    <div className='dest-info'>
                         <h3 className='dest-title header-3 fw-normal text-center'>{data[activeTab].name}</h3>
-                        <p className='dest-text body-text text-center'>
+                        <p className='dest-text body-text'>
                             {data[activeTab].description}
                         </p>
                     </div>
-                    <div className='dest-details-container mb-5 borderTop pt-4'>
-                        <div className='dest-details py-2'>
+                    <div className='dest-details container  borderTop d-flex flex-column'>
+                        <div>
                             <p className='dest-details-title subheading-2 text-center'>AVG. Temperature</p>
                             <p className='dest-details-data text-center data-text'>{data[activeTab].distance}</p>
                         </div>
-                        <div className='dest-details py-2'>
+                        <div>
                             <p className='dest-details-title subheading-2 text-center'>Est. Travel Time</p>
                             <p className='dest-details-data text-center data-text'>{data[activeTab].travel}</p>
                         </div>
--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -9,8 +9,8 @@ export default function Technology({ data }) {
         <section className="technology-page">
             <Header />
             <div className='d-flex flex-column justify-content-start align-items-center container'>
-                <h1 className='tech-page-title nav-text text-uppercase pb-2'>
-                    <span className='page-title-number nav-text opacity-25 '>03</span> Space launch 101
+                <h1 className='numbered-title nav-text text-uppercase'>
+                    <span aria-hidden='true' className='nav-text opacity-25 '>03</span> Space launch 101
                 </h1>
                 <article className='tech-image-container'>
                     <picture>
