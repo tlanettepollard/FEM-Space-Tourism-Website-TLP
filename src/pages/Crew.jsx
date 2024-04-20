0b528b4ca7416ef7b468e9c8cd0a5a0dac109bd8
T. Lanette <TLPollard4516@gmail.com>
2024-04-20 16:18:48 -0400

    Updates: crew page styling
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -8,12 +8,13 @@ export default function Crew({ data }) {
     return (
         <section className='crew-page container-fluid pb-1'>
             <Header />
-            <div className='crew-container container d-flex flex-column justify-content-evenly align-items-center  pb-2 pb-md-0'>
-                <h1 className='numbered-title nav-text text-uppercase text-center text-md-start w-100 pb-2 order-md-1'>
-                    <span aria-hidden='true' className='numbered-title opacity-25 pe-2'>02</span> Meet your crew
-                </h1>
-                <article className='crew-image-container order-md-3'>
-                    <picture className='crew-img pb-2 pb-md-0'>
+            <h1 className='numbered-title nav-text text-uppercase text-center text-md-start w-100'>
+                <span aria-hidden='true' className='numbered-title opacity-25 pe-2'>02</span> Meet your crew
+            </h1>
+            <div className='crew-container container d-flex flex-column flex-lg-row justify-content-evenly justify-lg-between align-items-center'>
+
+                <article className='crew-image-container order-md-3 flex-md-shrink-0'>
+                    <picture className='crew-img'>
                         <source media="(min-width: 30rem)" srcSet={data[activeTab].images.png} />
                         <img className='img-fluid mx-auto d-block borderBottom' src={data[activeTab].images.webp} alt={data[activeTab].name} />
                     </picture>
@@ -29,10 +30,10 @@ export default function Crew({ data }) {
                     </div>
                     <div className='crew-details order-md-1'>
                         <header>
-                            <h2 className='subheading-1 text-center crew-role'>{data[activeTab].role}</h2>
-                            <h3 className='subheading text-center crew-name'>{data[activeTab].name}</h3>
+                            <h2 className='subheading-1 text-center crew-role pb-3'>{data[activeTab].role}</h2>
+                            <h3 className='subheading text-center crew-name pb-4'>{data[activeTab].name}</h3>
                         </header>
-                        <p className='crew-bio body-text text-center px-4'>{data[activeTab].bio}</p>
+                        <p className='crew-bio body-text text-center px-4 px-md-5'>{data[activeTab].bio}</p>
                     </div>
                 </article>
 
