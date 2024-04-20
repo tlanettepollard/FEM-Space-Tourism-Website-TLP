f909813b44e265858c1a6aacb7230611f80ad0ca
T. Lanette <TLPollard4516@gmail.com>
2024-04-20 15:56:42 -0400

    Updates: added styling to crew page
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -6,26 +6,28 @@ export default function Crew({ data }) {
     const [activeTab, setActiveTab] = useState(0);
 
     return (
-        <section className='crew-page container-fluid'>
+        <section className='crew-page container-fluid pb-1'>
             <Header />
-            <div className='crew-container container'>
-                <h1 className='numbered-title nav-text text-uppercase text-center pb-2'>
-                    <span aria-hidden='true' className='nav-text opacity-25 pe-2'>02</span> Meet your crew
+            <div className='crew-container container d-flex flex-column justify-content-evenly align-items-center  pb-2 pb-md-0'>
+                <h1 className='numbered-title nav-text text-uppercase text-center text-md-start w-100 pb-2 order-md-1'>
+                    <span aria-hidden='true' className='numbered-title opacity-25 pe-2'>02</span> Meet your crew
                 </h1>
-                <article className='crew-image-container'>
-                    <picture className='crew-img'>
+                <article className='crew-image-container order-md-3'>
+                    <picture className='crew-img pb-2 pb-md-0'>
                         <source media="(min-width: 30rem)" srcSet={data[activeTab].images.png} />
                         <img className='img-fluid mx-auto d-block borderBottom' src={data[activeTab].images.webp} alt={data[activeTab].name} />
                     </picture>
                 </article>
-                <article className='crew-content container'>
-                    <ButtonContainer
-                        name={'crew'}
-                        data={data}
-                        activeTab={activeTab}
-                        setActiveTab={setActiveTab}
-                    />
-                    <div className='crew-details'>
+                <article className='crew-content container d-flex flex-column justify-content-center order-md-2'>
+                    <div className='order-md-2'>
+                        <ButtonContainer
+                            name={'crew'}
+                            data={data}
+                            activeTab={activeTab}
+                            setActiveTab={setActiveTab}
+                        />
+                    </div>
+                    <div className='crew-details order-md-1'>
                         <header>
                             <h2 className='subheading-1 text-center crew-role'>{data[activeTab].role}</h2>
                             <h3 className='subheading text-center crew-name'>{data[activeTab].name}</h3>
