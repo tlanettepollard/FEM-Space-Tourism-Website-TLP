68ae07c590c75080fc50225c820ddbc4496043b6
T. Lanette <TLPollard4516@gmail.com>
2024-06-15 20:43:11 -0400

    Updates: Destination page spacing
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/pages/Destinations.jsx
+++ src/pages/Destinations.jsx
@@ -7,12 +7,12 @@ export default function Destinations({ data }) {
     const [activeTab, setActiveTab] = useState(0);
 
     return (
-        <section className='destination-page container-fluid'>
+        <section className='destination-page container-fluid w-100 min-vh-100 m-auto'>
             <Header />
-            <main className='destination-main '>
-                <section className='destination-container container-xxl d-flex flex-column'>
+            <main className='destination-main w-100 h-auto'>
+                <section className='destination-container container-xxl'>
                     <div className='row'>
-                        <div className=''>
+                        <div className='destination-intro col-12 col-xl-6'>
                             <h1 className='numbered-title nav-text text-uppercase text-center text-md-start w-100 container'>
                                 <span aria-hidden='true' className='numbered-title opacity-25 pe-2
                     '>01</span> Pick your destination
@@ -25,31 +25,32 @@ export default function Destinations({ data }) {
                                 </picture>
                             </article>
                         </div>
+
+                        <article className='container destination-content col-12 col-xl-6'>
+                            <ButtonContainer
+                                name={'destinations'}
+                                data={data}
+                                activeTab={activeTab}
+                                setActiveTab={setActiveTab}
+                            />
+                            <div className='destination-info'>
+                                <h3 className='dest-title header-3 fw-normal text-center'>{data[activeTab].name}</h3>
+                                <p className='destination-text body-text text-center m-auto'>
+                                    {data[activeTab].description}
+                                </p>
+                            </div>
+                            <div className='destination-details container borderTop'>
+                                <div>
+                                    <p className='details-title subheading-2 text-center'>AVG. Temperature</p>
+                                    <p className='details-data text-center data-text'>{data[activeTab].distance}</p>
+                                </div>
+                                <div>
+                                    <p className='details-title subheading-2 text-center'>Est. Travel Time</p>
+                                    <p className='details-data text-center data-text'>{data[activeTab].travel}</p>
+                                </div>
+                            </div>
+                        </article>
                     </div>
-                    <article className='container destination-content'>
-                        <ButtonContainer
-                            name={'destinations'}
-                            data={data}
-                            activeTab={activeTab}
-                            setActiveTab={setActiveTab}
-                        />
-                        <div className='destination-info'>
-                            <h3 className='dest-title header-3 fw-normal text-center'>{data[activeTab].name}</h3>
-                            <p className='destination-text body-text text-center m-auto'>
-                                {data[activeTab].description}
-                            </p>
-                        </div>
-                        <div className='destination-details container borderTop'>
-                            <div>
-                                <p className='details-title subheading-2 text-center'>AVG. Temperature</p>
-                                <p className='details-data text-center data-text'>{data[activeTab].distance}</p>
-                            </div>
-                            <div>
-                                <p className='details-title subheading-2 text-center'>Est. Travel Time</p>
-                                <p className='details-data text-center data-text'>{data[activeTab].travel}</p>
-                            </div>
-                        </div>
-                    </article>
                 </section>
             </main>
         </section>
